2d632777a4e58570c0a87014a6c86aac70c78855
rinaldipp <rinaldipp@gmail.com>
2022-05-05 18:47:24 -0300

    Fix MKL load for newer versions.

--- pyMKL/loadMKL.py
+++ pyMKL/loadMKL.py
@@ -1,30 +1,39 @@
+import ctypes
 from ctypes import CDLL, RTLD_GLOBAL
+from ctypes.util import find_library
 import sys, os
+import glob
 
 platform = sys.platform
 
-libname = {'linux':'libmkl_rt.so', # works for python3 on linux
-           'linux2':'libmkl_rt.so', # works for python2 on linux
-           'darwin':'libmkl_rt.dylib',
-           'win32':'mkl_rt.dll'}
+libname = {'linux': 'libmkl_rt.so',  # works for python3 on linux
+           'linux2': 'libmkl_rt.so',  # works for python2 on linux
+           'darwin': 'libmkl_rt.dylib',
+           'win32': 'mkl_rt.dll'}
+
 
 def _loadMKL():
-    
-    try:
-        # Look for MKL in path
-        MKLlib = CDLL(libname[platform])
-    except:
-        try:
-            # Look for anaconda mkl
-            if 'Anaconda' in sys.version:
-                if platform in ['linux', 'linux2','darwin']:
-                    libpath = ['/']+sys.executable.split('/')[:-2] + \
-                              ['lib',libname[platform]]
-                elif platform == 'win32':
-                    libpath = sys.executable.split(os.sep)[:-1] + \
-                              ['Library','bin',libname[platform]]
-                MKLlib = CDLL(os.path.join(*libpath))
-        except Exception as e: 
-            raise e
+    libmkl = None
+    mkl_rt = find_library('mkl_rt')
+    if mkl_rt is None:
+        mkl_rt = find_library('mkl_rt.1')
 
+    if mkl_rt is None:
+        mkl_rt_path = sorted(
+            glob.glob(f'{sys.prefix}/[Ll]ib*/**/*mkl_rt*', recursive=True),
+            key=len
+        )
+        for path in mkl_rt_path:
+            try:
+                libmkl = ctypes.CDLL(path)
+                break
+            except (OSError, ImportError):
+                pass
+
+        if libmkl is None:
+            raise ImportError('mkl_rt not found')
+    else:
+        libmkl = ctypes.CDLL(mkl_rt)
+
+    MKLlib = libmkl
     return MKLlib
